bb788bb1a4dc93b3163e7b799e4318879592b532
omggomb <omggomb@gmx.de>
2016-04-11 19:42:07 +0200

    1. Copied tb_static project, so there's no need to change it everytime we update the submodule 2. Moved custom tb_config.h to project folder so it can be version controlled without hassle 3. Modified FreeImage.h in 64bit distr to resolve name conflict (we use compiled version only so there's not much impact)

--- FreeImage/Dist/x64/FreeImage.h
+++ FreeImage/Dist/x64/FreeImage.h
@@ -502,7 +502,7 @@ Constants used in FreeImage_Rescale.
 FI_ENUM(FREE_IMAGE_FILTER) {
 	FILTER_BOX		  = 0,	//! Box, pulse, Fourier window, 1st order (constant) b-spline
 	FILTER_BICUBIC	  = 1,	//! Mitchell & Netravali's two-param cubic filter
-	FILTER_BILINEAR   = 2,	//! Bilinear filter
+	FILTER_BILINEAR_F   = 2,	//! Bilinear filter
 	FILTER_BSPLINE	  = 3,	//! 4th order (cubic) b-spline
 	FILTER_CATMULLROM = 4,	//! Catmull-Rom spline, Overhauser spline
 	FILTER_LANCZOS3	  = 5	//! Lanczos3 filter

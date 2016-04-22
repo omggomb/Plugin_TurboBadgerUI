20f8e830f0dd62df5a5b726314770bfd20263da7
omggomb <omggomb@gmx.de>
2016-04-22 06:52:37 +0200

    Some testing renderer implementation (alpha state)

---
+++ inc/CryTBRendererEx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <renderers\tb_renderer_batcher.h>
+
+class CCryTBRendererEx : public tb::TBRendererBatcher
+{
+public:
+	CCryTBRendererEx();
+	~CCryTBRendererEx();
+
+	virtual TBBitmap *CreateBitmap(int width, int height, uint32 *data) = 0;
+	virtual void RenderBatch(Batch *batch) = 0;
+	virtual void SetClipRect(const TBRect &rect) = 0;
+};
---
+++ src/CryTBRendererEx.cpp
@@ -0,0 +1,81 @@
+#include <StdAfx.h>
+#include "CryTBBitmap.h"
+#include "CryTBRendererEx.h"
+
+tb::TBBitmap * CCryTBRendererEx::CreateBitmap(int width, int height, tb::uint32 * data)
+{
+	auto pBM = new CCryTBBitmap(width, height);
+	pBM->SetData(data);
+	return pBM;
+}
+
+CCryTBRendererEx::CCryTBRendererEx()
+{
+}
+
+CCryTBRendererEx::~CCryTBRendererEx()
+{
+}
+
+void CCryTBRendererEx::BeginPaint(int render_target_w, int render_target_h)
+{
+}
+
+void CCryTBRendererEx::EndPaint()
+{
+}
+
+void CCryTBRendererEx::Translate(int dx, int dy)
+{
+}
+
+void CCryTBRendererEx::SetOpacity(float opacity)
+{
+}
+
+float CCryTBRendererEx::GetOpacity()
+{
+	return 0.0f;
+}
+
+tb::TBRect CCryTBRendererEx::SetClipRect(const tb::TBRect & rect, bool add_to_current)
+{
+	return tb::TBRect();
+}
+
+tb::TBRect CCryTBRendererEx::GetClipRect()
+{
+	return tb::TBRect();
+}
+
+void CCryTBRendererEx::DrawBitmap(const tb::TBRect & dst_rect, const tb::TBRect & src_rect, tb::TBBitmapFragment * bitmap_fragment)
+{
+}
+
+void CCryTBRendererEx::DrawBitmap(const tb::TBRect & dst_rect, const tb::TBRect & src_rect, tb::TBBitmap * bitmap)
+{
+}
+
+void CCryTBRendererEx::DrawBitmapColored(const tb::TBRect & dst_rect, const tb::TBRect & src_rect, const tb::TBColor & color, tb::TBBitmapFragment * bitmap_fragment)
+{
+}
+
+void CCryTBRendererEx::DrawBitmapColored(const tb::TBRect & dst_rect, const tb::TBRect & src_rect, const tb::TBColor & color, tb::TBBitmap * bitmap)
+{
+}
+
+void CCryTBRendererEx::DrawBitmapTile(const tb::TBRect & dst_rect, tb::TBBitmap * bitmap)
+{
+}
+
+void CCryTBRendererEx::DrawRect(const tb::TBRect & dst_rect, const tb::TBColor & color)
+{
+}
+
+void CCryTBRendererEx::DrawRectFill(const tb::TBRect & dst_rect, const tb::TBColor & color)
+{
+}
+
+void CCryTBRendererEx::FlushBitmapFragment(tb::TBBitmapFragment * bitmap_fragment)
+{
+}
